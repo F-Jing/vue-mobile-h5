a0a392575dc78ddcc76da198e9ae25f633cb0999
F-Jing <jfjf7617123@163.com>
2020-10-11 16:36:23 +0800

    my first submit

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home-page">
+    <div class="card-list">
+      <div class="card-item"></div>
+      <div class="card-item"></div>
+      <div class="card-item"></div>
+    </div>
+    <van-button type="warning">默认按钮</van-button>
+  </div>
+</template>
+
+<script>
+
+import {Button} from "vant";
+
+export default {
+  components: {
+    "van-button": Button
+  }
+};
+</script>
+<style type="text/css" lang="less">
+.card-item{
+  display: inline-block;
+  width: 375/2px;
+  height: 20px;
+  vertical-align: top;
+  margin-bottom: 10px;
+  background: #f1f1f1;
+  font-size: 14px;
+}
+</style>
